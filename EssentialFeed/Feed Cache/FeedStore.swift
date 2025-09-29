d09792e3028df011bce460e1e884afeac4f75408
Bryan Yong <bryanywy@gmail.com>
2025-09-29 09:50:08 +0800

    Add typealias for save result error enum

---
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Bryan Yong on 29/09/2025.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
