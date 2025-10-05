66d9174f1ee3a96ced0c34ba5d7132b3aed17d4f
Bryan Yong <bryanywy@gmail.com>
2025-10-05 12:26:45 +0800

    Test validate cache should delete local if invalid

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -40,7 +40,6 @@ public class LocalFeedLoader {
             case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
             case .found:
-                self.store.deleteCachedFeed { _ in }
                 completion(.success([]))
             case .empty:
                 completion(.success([]))
@@ -52,6 +51,8 @@ public class LocalFeedLoader {
         store.retrieve { [unowned self] result in
             switch result {
             case .failure:
+                self.store.deleteCachedFeed { _ in }
+            case let .found(_, timestamp) where !self.validate(timestamp):
                 self.store.deleteCachedFeed { _ in }
             default: break
             }
