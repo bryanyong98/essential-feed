9fc278e8c5143599eb6deb8060273e1b0464ff80
Bryan Yong <bryanywy@gmail.com>
2025-10-28 09:38:34 +0800

    Add unit tests for FeedViewController

---
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -0,0 +1,99 @@
+//
+//  FeedViewControllerTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Bryan Yong on 27/10/2025.
+//
+
+import EssentialFeed
+import UIKit
+import XCTest
+
+final class FeedViewController: UITableViewController {
+    private var loader: FeedViewControllerTests.LoaderSpy?
+
+    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+        self.init()
+        self.loader = loader
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        refreshControl?.beginRefreshing()
+        load()
+    }
+
+    @objc private func load() {
+        loader?.load { _ in }
+    }
+}
+
+final class FeedViewControllerTests: XCTestCase {
+
+    class LoaderSpy: FeedLoader {
+
+        private(set) var loadCallCount: Int = 0
+
+        func load(completion: @escaping (EssentialFeed.LoadFeedResult) -> Void) {
+            loadCallCount += 1
+        }
+    }
+
+    func test_init_doesNotLoadFeed() {
+        let (_, loader) = makeSUT()
+
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+
+    func test_viewDidLoad_loadsFeed() {
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+
+    func test_pullToRefresh_loadsFeed() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+
+        sut.refreshControl?.simulatePullToRefresh()
+        XCTAssertEqual(loader.loadCallCount, 2)
+
+        sut.refreshControl?.simulatePullToRefresh()
+        XCTAssertEqual(loader.loadCallCount, 3)
+    }
+
+    func test_viewDidLoad_showsLoadingIndicator() {
+        let (sut, _) = makeSUT()
+
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
+    }
+}
+
+extension FeedViewControllerTests {
+
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        trackForMemoryLeaks(loader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+
+        return (sut, loader)
+    }
+}
+
+private extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
