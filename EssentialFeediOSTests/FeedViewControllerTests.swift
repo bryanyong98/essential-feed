dcac131de800f300c3a070cba0bbb172ddbd4d12
Bryan Yong <bryanywy@gmail.com>
2025-10-28 10:04:28 +0800

    Extract FeedViewController into separate file

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -6,32 +6,9 @@
 //
 
 import EssentialFeed
+import EssentialFeediOS
 import UIKit
 import XCTest
-
-final class FeedViewController: UITableViewController {
-    private var loader: FeedViewControllerTests.LoaderSpy?
-
-    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
-        self.init()
-        self.loader = loader
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
-        load()
-    }
-
-    @objc private func load() {
-        refreshControl?.beginRefreshing()
-        loader?.load { [weak self] _ in
-            self?.refreshControl?.endRefreshing()
-        }
-    }
-}
 
 private extension FeedViewController {
     func simulateUserInitiatedFeedReload() {
