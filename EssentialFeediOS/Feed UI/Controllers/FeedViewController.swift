018962fa539faa87969687295090d8f6d29fc2c7
Bryan Yong <bryanywy@gmail.com>
2025-11-16 12:22:39 +0800

    Extract dependency creation away from FeedVC and move into FeedUIComposer

---
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -0,0 +1,60 @@
+//
+//  FeedViewController.swift
+//  EssentialFeediOS
+//
+//  Created by Bryan Yong on 28/10/2025.
+//
+
+import Foundation
+import UIKit
+import EssentialFeed
+
+final public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
+    private var refreshController: FeedRefreshViewController?
+    var tableModel = [FeedImageCellController]() {
+        didSet { tableView.reloadData() }
+    }
+
+    convenience init(refreshController: FeedRefreshViewController) {
+        self.init()
+        self.refreshController = refreshController
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        refreshControl = refreshController?.view
+        tableView.prefetchDataSource = self
+        refreshController?.refresh()
+    }
+
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableModel.count
+    }
+
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return cellController(forRowAt: indexPath).view()
+    }
+
+    public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        removeCellController(forRowAt: indexPath)
+    }
+
+    public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        indexPaths.forEach { indexPath in
+            cellController(forRowAt: indexPath).preload()
+        }
+    }
+
+    public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
+        indexPaths.forEach(removeCellController)
+    }
+
+    private func cellController(forRowAt indexPath: IndexPath) -> FeedImageCellController {
+        return tableModel[indexPath.row]
+    }
+
+    private func removeCellController(forRowAt indexPath: IndexPath) {
+        cellController(forRowAt: indexPath).cancelLoad()
+    }
+}
